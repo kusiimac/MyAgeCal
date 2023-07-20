5611cfcef7d714d37096b438465710337a661393
kusiimac <cleophaskusiima@gmail.com>
2023-07-20 19:57:43 +0300

    My age cal

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+/**
+ * Write a program that calculates the difference between the year you were born and the current year
+ */
+fun main() {
+    //This program will calculate my real age
+    val x = 1990
+    val y = 2023
+    val z = y - x
+    print("My age is $z")
+}
